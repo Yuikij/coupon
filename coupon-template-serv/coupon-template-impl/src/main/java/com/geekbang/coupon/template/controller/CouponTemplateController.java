3ab0dd6d695f308b858fbde9cdf3aa6c72f15dd6
zxj <775718540@qq.com>
2023-03-15 15:19:43 +0800

    end sentinel

--- coupon-template-serv/coupon-template-impl/src/main/java/com/geekbang/coupon/template/controller/CouponTemplateController.java
+++ coupon-template-serv/coupon-template-impl/src/main/java/com/geekbang/coupon/template/controller/CouponTemplateController.java
@@ -1,11 +1,13 @@
 package com.geekbang.coupon.template.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
 import com.geekbang.coupon.template.api.beans.CouponTemplateInfo;
 import com.geekbang.coupon.template.api.beans.PagedCouponTemplateInfo;
 import com.geekbang.coupon.template.api.beans.TemplateSearchParams;
 import com.geekbang.coupon.template.service.intf.CouponTemplateService;
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.sql.Template;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,11 +52,26 @@ public class CouponTemplateController {
     // 你也可以通过defaultFallback属性做一个全局限流、降级的处理逻辑
     // 如果你不想将降级方法写在当前类里，可以通过blockHandlerClass和fallbackClass指定"降级类"
     @SentinelResource(value = "getTemplateInBatch",
+            //可以在其他RuntimeException 的情况下执行降级逻辑
             fallback = "getTemplateInBatch_fallback",
+            //在它只能在服务抛出 BlockException的情况下执行降级逻辑
             blockHandler = "getTemplateInBatch_block")
     public Map<Long, CouponTemplateInfo> getTemplateInBatch(@RequestParam("ids") Collection<Long> ids) {
         log.info("getTemplateInBatch: {}", JSON.toJSONString(ids));
         return couponTemplateService.getTemplateInfoMap(ids);
+    }
+
+    // 接口被降级时的方法
+    public Map<Long, CouponTemplateInfo> getTemplateInBatch_fallback(Collection<Long> ids) {
+        log.info("接口被降级");
+        return Maps.newHashMap();
+    }
+
+    // 流控降级的方法
+    public Map<Long, CouponTemplateInfo> getTemplateInBatch_block(
+            Collection<Long> ids, BlockException e) {
+        log.info("接口被限流");
+        return Maps.newHashMap();
     }
 
     // 搜索模板
